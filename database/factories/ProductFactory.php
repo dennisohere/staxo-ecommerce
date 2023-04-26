73a0bae917e486e2b725d70453c58fc0e833a28d
Dennis Ohere <dennisohere@live.com>
2023-04-26 04:49:22 +0100

    product db

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name = $this->faker->words(2,true);
+        return [
+            'name' => $name,
+            'price' => $this->faker->numberBetween(30, 130),
+            'slug' => Str::slug($name),
+        ];
+    }
+}
